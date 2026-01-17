daaa99fbc469d14265d6aa788b7c74d36de8065f
Stefan <stefan_riley@icloud.com>
2026-01-16 21:47:13 -0500

    kafka is running

--- src/main/java/com/stefan/BudgetSavingService/BudgetSavingServiceApplication.java
+++ src/main/java/com/stefan/BudgetSavingService/BudgetSavingServiceApplication.java
@@ -1,14 +1,27 @@
 package com.stefan.BudgetSavingService;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class BudgetSavingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetSavingServiceApplication.class, args);
-
 	}
+
+	//For inital Testing
+//	@Bean
+//	CommandLineRunner runner(KafkaTemplate<String, String> kafkaTemplate) {
+//		return args -> {
+//			for (int i = 0; i < 10; i++) {
+//				kafkaTemplate.send("BudgetSavingService", "Budget Saving Service");
+//
+//			}
+//		};
+//	}
 
 }
